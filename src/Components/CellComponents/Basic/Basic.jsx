536ae7573fd40c354d737b080e3bb6d25ed56e70
Yash Bhalodi <yashbhalodi@toddleapp.com>
2022-06-11 22:10:25 +0530

    fix: minor UI update in BASIC cell type

--- src/Components/CellComponents/Basic/Basic.jsx
+++ src/Components/CellComponents/Basic/Basic.jsx
@@ -21,19 +21,24 @@ const Basic = ({ cellId }) => {
   return (
     <td>
       <div
-        className="group hover:bg-blue-800 hover:shadow-xl relative flex flex-col w-56 h-24 px-4 py-4 text-gray-100 transition-all bg-blue-900 rounded-md"
+        className="group hover:bg-blue-800 hover:shadow-xl relative flex flex-col items-start justify-center w-56 h-24 px-4 py-4 text-gray-100 transition-all bg-blue-900 rounded-md"
         onMouseEnter={setMouseEnter}
         onMouseLeave={setMouseLeave}
       >
-        <textarea
-          type={"text"}
-          name="title"
-          value={title}
-          readOnly={!isEditMode}
-          onChange={handleTextChange}
-          rows={3}
-          className="group-hover:bg-blue-800 scrollbar text-gray-100 transition-all bg-blue-900 outline-none resize-none"
-        />
+        {isEditMode ? (
+          <textarea
+            type={"text"}
+            name="title"
+            value={title}
+            onChange={handleTextChange}
+            rows={3}
+            className="group-hover:bg-blue-800 scrollbar text-gray-100 transition-all bg-blue-900 outline-none resize-none"
+          />
+        ) : (
+          <div className="scrollbar overflow-auto whitespace-pre-line select-all">
+            {title}
+          </div>
+        )}
         {isEditMode && showResetButton && (
           <FiTrash
             className="top-2 right-2 hover:text-red-300 absolute text-xl text-red-100 transition-all rounded-md"
