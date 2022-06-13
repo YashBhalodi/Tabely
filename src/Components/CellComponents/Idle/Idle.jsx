ed372f4d6c48eebb2ee43649f9a5db337065640a
Yash Bhalodi <yashbhalodi@toddleapp.com>
2022-06-13 23:56:58 +0530

    feat: added keyboard action for idle cells

--- src/Components/CellComponents/Idle/Idle.jsx
+++ src/Components/CellComponents/Idle/Idle.jsx
@@ -23,9 +23,16 @@ const Idle = ({ cellId }) => {
     isEditMode && setShouldShowText(false);
   };
 
+  const handleKeyboardEvent = (e) => {
+    if (e.code === "Enter") {
+      handleClick();
+    }
+  };
+
   return (
     <td key={cellId}>
       <div
+        tabIndex={isEditMode ? 0 : -1}
         className={`bg-slate-10 hover:border-2 flex flex-col items-center justify-center min-w-[14rem] min-h-[6rem] p-2 transition-all rounded-md shadow-sm border border-1 border-slate-200 ${
           isEditMode
             ? "hover:border-blue-300 hover:bg-slate-50 cursor-pointer"
@@ -34,6 +41,7 @@ const Idle = ({ cellId }) => {
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
         onClick={handleClick}
+        onKeyDownCapture={handleKeyboardEvent}
       >
         {shouldShowText && <FiPlusCircle className="text-xl text-blue-300" />}
       </div>
