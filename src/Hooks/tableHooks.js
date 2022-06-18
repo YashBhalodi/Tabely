b32247eb30f11fa4ea33909db5035a325acb42e7
Yash Bhalodi <yashbhalodi@toddleapp.com>
2022-06-18 22:02:07 +0530

    feat: utility to relocate a cell in table

--- src/Hooks/tableHooks.js
+++ src/Hooks/tableHooks.js
@@ -2,20 +2,86 @@ import { useRecoilState, useResetRecoilState } from "recoil";
 import { tableAtom, cellsSelector } from "Atoms";
 import _ from "lodash";
 
+const getColumnBasedLayout = (allRows) => {
+  const result = [];
+  for (let i = 0; i < allRows[0].length; i++) {
+    const currentColumn = [];
+    allRows.forEach((row) => currentColumn.push(row[i]));
+    result.push(currentColumn);
+  }
+  return result;
+};
+
+const getRowBasedLayout = (allColumns) => {
+  const result = [];
+  for (let i = 0; i < allColumns[0].length; i++) {
+    const currentRow = [];
+    allColumns.forEach((column) => currentRow.push(column[i]));
+    result.push(currentRow);
+  }
+  return result;
+};
+
+const getCellIdPosition = ({ layout, cellId }) => {
+  const rowArray = _.find(layout, (row) => _.includes(row, cellId));
+  const column = _.indexOf(rowArray, cellId);
+  const row = _.indexOf(layout, rowArray);
+  return { row, column };
+};
+
+const getCellRelocatedLayout = ({
+  initialLayout,
+  cellId,
+  targetCellId,
+  direction = "above",
+}) => {
+  const columnLayout = getColumnBasedLayout(initialLayout);
+  const { row, column } = getCellIdPosition({ layout: columnLayout, cellId });
+  const { row: targetRow, column: targetColumn } = getCellIdPosition({
+    layout: columnLayout,
+    cellId: targetCellId,
+  });
+
+  if (targetRow === row) {
+    // same column relocation
+    // no need to add new row to table
+    const indexOfCellId = columnLayout[row].indexOf(cellId);
+    columnLayout[targetRow].splice(indexOfCellId, 1);
+    const indexOfTargetCellId = columnLayout[row].indexOf(targetCellId);
+    const splicedInIndex =
+      direction === "above" ? indexOfTargetCellId : indexOfTargetCellId + 1;
+    columnLayout[targetRow].splice(splicedInIndex, 0, cellId);
+    const rowLayout = getRowBasedLayout(columnLayout);
+    return rowLayout;
+  } else {
+    const indexOfCellId = columnLayout[row].indexOf(cellId);
+    columnLayout[row].splice(indexOfCellId, 1);
+    const indexOfTargetCellId = columnLayout[targetRow].indexOf(targetCellId);
+    const splicedInIndex =
+      direction === "above" ? indexOfTargetCellId : indexOfTargetCellId + 1;
+    columnLayout[targetRow].splice(splicedInIndex, 0, cellId);
+
+    columnLayout.forEach((column, index) => {
+      if (index !== targetRow) {
+        if (index === row) {
+          column.push(_.uniqueId(), _.uniqueId());
+        } else {
+          column.push(_.uniqueId());
+        }
+      }
+    });
+    const rowLayout = getRowBasedLayout(columnLayout);
+    return rowLayout;
+  }
+};
+
 export const useTable = () => {
   const [allRows, setTableRows] = useRecoilState(tableAtom);
   const resetTableState = useResetRecoilState(tableAtom);
   const [__, updateSelectedCells] = useRecoilState(cellsSelector);
 
-  const getCellIdPosition = (cellId) => {
-    const rowArray = _.find(allRows, (row) => _.includes(row, cellId));
-    const column = _.indexOf(rowArray, cellId);
-    const row = _.indexOf(allRows, rowArray);
-    return { row, column };
-  };
-
   const getCellEdgePosition = (cellId) => {
-    const { row, column } = getCellIdPosition(cellId);
+    const { row, column } = getCellIdPosition({ layout: allRows, cellId });
     let edges = [];
     if (row === 0) {
       edges.push("top");
@@ -53,7 +119,7 @@ export const useTable = () => {
 
       let splicedInIndex = newAllRows.length;
       if (cellId) {
-        const { row } = getCellIdPosition(cellId);
+        const { row } = getCellIdPosition({ layout: prevAllRows, cellId });
         splicedInIndex = position === "below" ? row + 1 : row;
       }
 
@@ -74,7 +140,7 @@ export const useTable = () => {
 
       let splicedInIndex = newAllRows[0].length;
       if (cellId) {
-        const { column } = getCellIdPosition(cellId);
+        const { column } = getCellIdPosition({ layout: prevAllRows, cellId });
         splicedInIndex = position === "left" ? column : column + 1;
       }
       newAllRows.forEach((row) => {
@@ -135,13 +201,31 @@ export const useTable = () => {
 
   const swapCells = (cellId1, cellId2) => {
     setTableRows((prevAllRows) => {
-      const { row: row1, column: column1 } = getCellIdPosition(cellId1);
-      const { row: row2, column: column2 } = getCellIdPosition(cellId2);
+      const { row: row1, column: column1 } = getCellIdPosition({
+        layout: prevAllRows,
+        cellId: cellId1,
+      });
+      const { row: row2, column: column2 } = getCellIdPosition({
+        layout: prevAllRows,
+        cellId: cellId2,
+      });
       const newAllRows = _.cloneDeep(prevAllRows);
       const temp = newAllRows[row1][column1];
       newAllRows[row1][column1] = newAllRows[row2][column2];
       newAllRows[row2][column2] = temp;
       return newAllRows;
+    });
+  };
+
+  const relocateCell = ({ cellId, targetCellId, direction = "above" }) => {
+    setTableRows((prevAllRows) => {
+      const updatedLayout = getCellRelocatedLayout({
+        initialLayout: prevAllRows,
+        cellId,
+        targetCellId,
+        direction,
+      });
+      return updatedLayout;
     });
   };
 
@@ -155,5 +239,6 @@ export const useTable = () => {
     deleteRow,
     getCellEdgePosition,
     swapCells,
+    relocateCell,
   };
 };
