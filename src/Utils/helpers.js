ab5cd84f95ff1f2cad085f08fff7024b5a6a6af4
Yash Bhalodi <yashbhalodi@toddleapp.com>
2022-06-23 22:56:27 +0530

    chore: added a critical comment

--- src/Utils/helpers.js
+++ src/Utils/helpers.js
@@ -3,5 +3,6 @@ import ShortUniqueId from "short-unique-id";
 const uid = new ShortUniqueId({ length: 6 });
 
 export const getUniqId = () => {
+  // IMPORTANT any changes in ID delivery mechanism should be accompanied by a mechanism to migrate existing user's data
   return uid();
 };
