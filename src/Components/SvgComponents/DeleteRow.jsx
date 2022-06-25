fc8f3b3ca271587447a3038219b022f10f88b0f4
Yash Bhalodi <yashbhalodi@toddleapp.com>
2022-06-25 23:27:13 +0530

    feat: added custom svg icon components

---
+++ src/Components/SvgComponents/DeleteRow.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const DeleteRow = (props) => {
+  const {
+    width = 24,
+    height = 24,
+    containerClass = "",
+    pathClass = "fill-black",
+  } = props;
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={containerClass}
+    >
+      <path
+        stroke="#000"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        d="M2.75 19.424h18.5"
+        className={pathClass}
+      />
+      <g
+        clip-path="url(#a)"
+        stroke="#000"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className={pathClass}
+      >
+        <path
+          d="M6.75 7.326h10.5m-1.167 0v8.167a1.167 1.167 0 0 1-1.166 1.167H9.083a1.167 1.167 0 0 1-1.166-1.167V7.326m1.75 0V6.16a1.167 1.167 0 0 1 1.166-1.167h2.334a1.167 1.167 0 0 1 1.166 1.167v1.166"
+          className={pathClass}
+        />
+      </g>
+      <defs>
+        <clipPath id="a">
+          <path
+            fill="#fff"
+            transform="translate(5 3.826)"
+            d="M0 0h14v14H0z"
+            className={pathClass}
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default DeleteRow;
---
+++ src/Components/SvgComponents/index.js
@@ -0,0 +1,15 @@
+import AddColumnLeft from "./AddColumnLeft";
+import AddColumnRight from "./AddColumnRight";
+import AddRowAbove from "./AddRowAbove";
+import AddRowBelow from "./AddRowBelow";
+import DeleteColumn from "./DeleteColumn";
+import DeleteRow from "./DeleteRow";
+
+export {
+  AddColumnLeft,
+  AddColumnRight,
+  AddRowAbove,
+  AddRowBelow,
+  DeleteColumn,
+  DeleteRow,
+};
