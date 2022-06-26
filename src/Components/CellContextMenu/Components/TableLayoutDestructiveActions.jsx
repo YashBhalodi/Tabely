b50d116924c61c4d79d6b91825ba9c456754bc6d
Yash Bhalodi <yashbhalodi@toddleapp.com>
2022-06-26 11:24:57 +0530

    faet: table layout destructive actions context menu

--- src/Components/CellContextMenu/Components/TableLayoutDestructiveActions.jsx
+++ src/Components/CellContextMenu/Components/TableLayoutDestructiveActions.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useBoard, useTable } from "Hooks";
 
+import { DeleteColumn, DeleteRow } from "Components/SvgComponents";
+
 const TableLayoutDestructiveActions = (props) => {
   const { cellId } = props;
   const { boardId } = useParams();
   const { tableId } = useBoard({ id: boardId });
-  const { getCellEdgePosition } = useTable({ id: tableId });
+  const { getCellEdgePosition, deleteRow, deleteColumn } = useTable({
+    id: tableId,
+  });
   const { row, column } = getCellEdgePosition(cellId);
 
   const handleClick = (action) => {
@@ -23,7 +27,22 @@ const TableLayoutDestructiveActions = (props) => {
     }
   };
 
-  return <div>Delete</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <div
+        onClick={() => handleClick("delete_column")}
+        className="hover:bg-slate-100 h-fit w-fit p-1 transition-colors border rounded-md"
+      >
+        <DeleteColumn />
+      </div>
+      <div
+        onClick={() => handleClick("delete_row")}
+        className="hover:bg-slate-100 h-fit w-fit p-1 transition-colors border rounded-md"
+      >
+        <DeleteRow pathClass="fill-red-700" />
+      </div>
+    </div>
+  );
 };
 
 export default TableLayoutDestructiveActions;
