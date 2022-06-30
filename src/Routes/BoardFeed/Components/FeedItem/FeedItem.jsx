f38d23e9d8af27f319dd9c9db37e8109b5a06c78
Yash Bhalodi <yashbhalodi@toddleapp.com>
2022-06-30 21:24:07 +0530

    fix: minor accessibility improvement

--- src/Routes/BoardFeed/Components/FeedItem/FeedItem.jsx
+++ src/Routes/BoardFeed/Components/FeedItem/FeedItem.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 import { FiChevronRight, FiTrash } from "react-icons/fi";
 
@@ -50,14 +50,16 @@ const FeedItem = (props) => {
 
   return (
     <div className={`group relative ${isDragging ? "opacity-0" : ""}`}>
-      <div
-        className={`shadow-blue-100 hover:bg-blue-50 hover:border hover:border-blue-200 bg-blue-100/50 flex flex-row items-center justify-center px-8 py-4 space-x-4 text-lg text-blue-900 transition-all duration-200 border border-transparent rounded-md shadow-sm cursor-pointer`}
-        onClick={handleClick}
-        ref={drag(drop(ref))}
-      >
-        <div className="flex-1 truncate">{title || "Untitled board"}</div>
-        <FiChevronRight className="text-2xl" />
-      </div>
+      <Link to={`${id}`}>
+        <div
+          className={`shadow-blue-100 hover:bg-blue-50 hover:border hover:border-blue-200 bg-blue-100/50 flex flex-row items-center justify-center px-8 py-4 space-x-4 text-lg text-blue-900 transition-all duration-200 border border-transparent rounded-md shadow-sm cursor-pointer`}
+          onClick={handleClick}
+          ref={drag(drop(ref))}
+        >
+          <div className="flex-1 truncate">{title || "Untitled board"}</div>
+          <FiChevronRight className="text-2xl" />
+        </div>
+      </Link>
       <div className="group-hover:visible group-hover:opacity-100 group-hover:-translate-x-12 top-1/2 absolute flex flex-row-reverse items-center justify-center invisible h-full pr-8 space-x-4 transition-all transform -translate-y-1/2 opacity-0">
         <FiTrash
           className="hover:text-red-700 text-xl text-red-300 transition-colors cursor-pointer"
