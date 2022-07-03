db1c5698234b9b3c660ea6f00eb1206f74b2996b
Yash Bhalodi <yashbhalodi@toddleapp.com>
2022-07-03 10:40:55 +0530

    fix: minor

--- src/Components/CellComponents/Blank/Blank.jsx
+++ src/Components/CellComponents/Blank/Blank.jsx
@@ -35,7 +35,7 @@ const Blank = ({ cellId }) => {
       <TableActionsCellWrapper cellId={cellId}>
         <div
           tabIndex={isEditMode ? 0 : -1}
-          className={`group bg-transparent hover:border-2 flex flex-col items-center h-full w-full justify-center min-w-14 min-h-6 p-2 transition-all rounded-md hover:shadow-sm ${
+          className={`group bg-transparent hover:border-2 flex flex-col items-center h-full w-full justify-center min-w-cell min-h-cell p-2 transition-all rounded-md hover:shadow-sm ${
             isEditMode
               ? "hover:border-blue-300 hover:bg-slate-50 cursor-pointer"
               : ""
