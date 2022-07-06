cb2e5a2627b8fd7caaa844669f688a6d76d72b0e
Yash Bhalodi <yashbhalodi@toddleapp.com>
2022-07-06 22:44:47 +0530

    fix: date picker input ui improvement

--- src/Components/CellComponents/Card/Components/CellDatePicker.jsx
+++ src/Components/CellComponents/Card/Components/CellDatePicker.jsx
@@ -10,19 +10,8 @@ import { FiCalendar, FiXCircle } from "react-icons/fi";
 import { COLOR_THEME } from "Utils/colors";
 import "react-datepicker/dist/react-datepicker.css";
 
-const CellDatePicker = (props) => {
-  const { cellId } = props;
-  const { cellData, updateFields } = useCell({ id: cellId });
-  const { boardId } = useParams();
-  const { isEditMode } = useBoard({ id: boardId });
-  const { colorTheme, date } = cellData;
-  const themeItem = COLOR_THEME[colorTheme] || COLOR_THEME.STONE;
-
-  const handleDateChange = (e) => {
-    updateFields({ date: e });
-  };
-
-  const DateInput = forwardRef(({ value, onClick }, ref) => {
+const DateInput = forwardRef(
+  ({ themeItem, date, updateFields, isEditMode, onClick }, ref) => {
     const getDateString = () => {
       if (date) {
         const d = new Date(date);
@@ -38,18 +27,35 @@ const CellDatePicker = (props) => {
 
     return (
       <div
-        className={`flex flex-row space-x-2 items-center w-full min-h-[42px] rounded-md ${themeItem.lightBgColor} ${themeItem.lightBgBorderColor} ${themeItem.darkTextColor} border mix-blend-multiply p-2`}
+        className={`flex flex-row space-x-2 items-center w-full min-h-[42px] rounded-md ${themeItem.lightBgColor} ${themeItem.lightBgBorderColor} ${themeItem.darkTextColor} border mix-blend-multiply p-2 cursor-pointer hover:mix-blend-normal transition`}
         onClick={onClick}
         ref={ref}
       >
         <FiCalendar className={`text-xl opacity-30`} />
         <div className="flex-1">{getDateString()}</div>
         {getDateString() && isEditMode ? (
-          <FiXCircle className={`text-xl opacity-30`} onClick={clearDate} />
+          <FiXCircle
+            className={`text-xl opacity-30 hover:opacity-70`}
+            onClick={clearDate}
+          />
         ) : null}
       </div>
     );
-  });
+  }
+);
+DateInput.displayName = "DateInput";
+
+const CellDatePicker = (props) => {
+  const { cellId } = props;
+  const { cellData, updateFields } = useCell({ id: cellId });
+  const { boardId } = useParams();
+  const { isEditMode } = useBoard({ id: boardId });
+  const { colorTheme, date } = cellData;
+  const themeItem = COLOR_THEME[colorTheme] || COLOR_THEME.STONE;
+
+  const handleDateChange = (e) => {
+    updateFields({ date: e });
+  };
 
   const selectedDate = !isNaN(parseISO(date)) ? parseISO(date) : undefined;
 
@@ -57,10 +63,18 @@ const CellDatePicker = (props) => {
     <DatePicker
       selected={selectedDate}
       onChange={handleDateChange}
-      customInput={<DateInput />}
+      customInput={
+        <DateInput
+          themeItem={themeItem}
+          updateFields={updateFields}
+          date={date}
+          isEditMode={isEditMode}
+        />
+      }
       readOnly={!isEditMode}
     />
   );
 };
+CellDatePicker.displayName = "CellDatePicker";
 
 export default React.memo(CellDatePicker);
