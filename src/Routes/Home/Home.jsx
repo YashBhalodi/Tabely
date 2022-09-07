abfd638943a6bb8a08b2f818b94f782d6614717e
Yash Bhalodi <yashbhalodi@toddleapp.com>
2022-09-07 15:28:10 +0530

    dummy: netlify form testing

--- src/Routes/Home/Home.jsx
+++ src/Routes/Home/Home.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <form name="contact" netlify method="post">
+      <p>
+        <label>
+          Name <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Email <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  );
 };
 
 export default Home;
