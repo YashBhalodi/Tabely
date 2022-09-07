4b76479879e72bb4ee1d91b32e44035a9815093e
Yash Bhalodi <yashbhalodi@toddleapp.com>
2022-09-07 15:35:23 +0530

    dummy: netlify forms

--- src/Routes/Home/Home.jsx
+++ src/Routes/Home/Home.jsx
@@ -2,15 +2,21 @@ import React from "react";
 
 const Home = () => {
   return (
-    <form name="contact" netlify method="post">
+    <form name="contact" method="post">
+      <input type="hidden" name="form-name" value="contact" />
       <p>
         <label>
-          Name <input type="text" name="name" />
+          Your Name: <input type="text" name="name" />
         </label>
       </p>
       <p>
         <label>
-          Email <input type="email" name="email" />
+          Your Email: <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message"></textarea>
         </label>
       </p>
       <p>
