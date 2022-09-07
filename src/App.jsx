8c17cb71c329774a9eaa08ac67d5d9dac3dfd0b2
Yash Bhalodi <yashbhalodi@toddleapp.com>
2022-09-07 16:40:39 +0530

    fix: route based lazy loading code splitting

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,84 @@
+import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 
-import {
-  Home,
-  BoardFeed,
-  Board,
-  Contact,
-  RoadMap,
-  Updates,
-  NotFoundPage,
-} from "./Routes";
-
 import { NavBar } from "Components";
+
+const Home = React.lazy(() => import("./Routes/Home"));
+const BoardFeed = React.lazy(() => import("./Routes/BoardFeed"));
+const Board = React.lazy(() => import("./Routes/Board"));
+const Contact = React.lazy(() => import("./Routes/Contact"));
+const RoadMap = React.lazy(() => import("./Routes/Roadmap"));
+const Updates = React.lazy(() => import("./Routes/Updates"));
+const NotFoundPage = React.lazy(() => import("./Routes/404"));
+
+const SuspenseWrapper = ({ children }) => {
+  return <Suspense fallback={<div>Loading</div>}>{children}</Suspense>;
+};
 
 const App = () => {
   return (
     <div className="flex flex-col w-screen h-screen">
       <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/boards" element={<BoardFeed />} />
-        <Route path="/boards/:boardId" element={<Board />} />
-        <Route path="/updates" element={<Updates />} />
-        <Route path="/roadmap" element={<RoadMap />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <SuspenseWrapper>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <SuspenseWrapper>
+                <Home />
+              </SuspenseWrapper>
+            }
+          />
+          <Route
+            path="/boards"
+            element={
+              <SuspenseWrapper>
+                <BoardFeed />
+              </SuspenseWrapper>
+            }
+          />
+          <Route
+            path="/boards/:boardId"
+            element={
+              <SuspenseWrapper>
+                <Board />
+              </SuspenseWrapper>
+            }
+          />
+          <Route
+            path="/updates"
+            element={
+              <SuspenseWrapper>
+                <Updates />
+              </SuspenseWrapper>
+            }
+          />
+          <Route
+            path="/roadmap"
+            element={
+              <SuspenseWrapper>
+                <RoadMap />
+              </SuspenseWrapper>
+            }
+          />
+          <Route
+            path="/contact"
+            element={
+              <SuspenseWrapper>
+                <Contact />
+              </SuspenseWrapper>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <SuspenseWrapper>
+                <NotFoundPage />
+              </SuspenseWrapper>
+            }
+          />
+        </Routes>
+      </SuspenseWrapper>
     </div>
   );
 };
