5f6810d4105203295c9e569c23bc41630c2dd847
Yash Bhalodi <yashbhalodi@toddleapp.com>
2022-11-18 23:13:18 +0530

    fix: popover width

--- src/Routes/Board/Components/KeyBoardActionHandler/KeyBoardActionTooltip.jsx
+++ src/Routes/Board/Components/KeyBoardActionHandler/KeyBoardActionTooltip.jsx
@@ -85,7 +85,7 @@ const keyCombinations = [
 
 const KeyBoardKeyHelpOverlay = (props) => {
   return (
-    <div className="w-[40vw] h-[70vh] mr-5 bg-blue-50 border border-blue-100 rounded-lg shadow-md py-2 flex flex-col gap-4">
+    <div className="w-[30vw] h-[70vh] mr-5 bg-blue-50 border border-blue-100 rounded-lg shadow-md py-2 flex flex-col gap-4">
       <h5 className="text-blue-800/50 self-center text-lg font-medium">
         Keyboard shortcuts
       </h5>
