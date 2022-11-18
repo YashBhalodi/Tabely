3605a62554f72339019819d45b59df2b26fcb184
Yash Bhalodi <yashbhalodi@toddleapp.com>
2022-11-18 20:56:58 +0530

    fix: global search button keyboard keys ui

--- src/Components/GlobalSearchButton/GlobalSearchButton.jsx
+++ src/Components/GlobalSearchButton/GlobalSearchButton.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useKBar } from "kbar";
-import { FiSearch } from "react-icons/fi";
+import { FiCommand, FiSearch } from "react-icons/fi";
 import { Button } from "Components";
 
 const GlobalSearchButton = () => {
@@ -18,8 +18,17 @@ const GlobalSearchButton = () => {
     >
       <FiSearch />
       <span>Search . . .</span>
-      <span className="bg-blue-100/50 px-1 font-thin rounded">
-        <kbd>{isMac ? "cmd" : "ctrl"}</kbd>+<kbd>k</kbd>
+      <span className="bg-blue-100/50 flex flex-row items-center px-1 text-sm font-thin rounded">
+        {isMac ? (
+          <>
+            <FiCommand className="mr-1" />
+            <kbd>K</kbd>
+          </>
+        ) : (
+          <>
+            <kbd>ctrl</kbd>+<kbd>K</kbd>
+          </>
+        )}
       </span>
     </Button>
   );
