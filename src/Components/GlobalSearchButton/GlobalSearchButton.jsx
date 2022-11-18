a1cc9b1b0274250998384e80af235f2d51510e20
Yash Bhalodi <yashbhalodi@toddleapp.com>
2022-11-18 18:50:58 +0530

    fix: button padding for global search

--- src/Components/GlobalSearchButton/GlobalSearchButton.jsx
+++ src/Components/GlobalSearchButton/GlobalSearchButton.jsx
@@ -12,13 +12,13 @@ const GlobalSearchButton = () => {
       variant={"primary-plain"}
       shape={"square"}
       size={"md"}
-      className="text-blue-500/80 flex flex-row items-center justify-center gap-2 py-2 font-light"
+      className="text-blue-500/80 flex flex-row items-center justify-center gap-2 py-[12px] font-light"
       onClick={query.toggle}
       tabIndex={0}
     >
       <FiSearch />
       <span>Search . . .</span>
-      <span className="bg-blue-100/70 px-1 rounded">
+      <span className="bg-blue-100/50 px-1 rounded">
         <kbd>{isMac ? "cmd" : "ctrl"}</kbd>+<kbd>k</kbd>
       </span>
     </Button>
